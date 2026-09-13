6246893c61de326c1833abd6faa6dc48531b551d
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 10:28:11 +0530

    feat(home): compose future export direction into homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import { AboutSection } from '../../components/home/AboutSection'
 import { AnnouncementTicker } from '../../components/home/AnnouncementTicker'
 import { ContactSection } from '../../components/home/ContactSection'
 import { ExportProcess } from '../../components/home/ExportProcess'
+import { FutureVision } from '../../components/home/FutureVision'
 import { Hero } from '../../components/home/Hero'
 import { ProductGrid } from '../../components/home/ProductGrid'
 import { ValuesSection } from '../../components/home/ValuesSection'
@@ -15,6 +16,7 @@ export function Home() {
       <AboutSection />
       <ProductGrid />
       <ExportProcess />
+      <FutureVision />
       <ValuesSection />
       <ContactSection />
     </>
