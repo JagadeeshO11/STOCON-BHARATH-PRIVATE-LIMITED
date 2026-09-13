daa6dc1a1d8327ef243df382313690d317cdfc1d
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 09:51:45 +0530

    fix: align modules with centralized company configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Header } from './components/layout/Header'
 import { Footer } from './components/layout/Footer'
+import { Header } from './components/layout/Header'
 import { Home } from './pages/Home/Home'
 
 function App() {
-  return <><Header/><main><Home/></main><Footer/></>
+  return (
+    <>
+      <Header />
+      <main><Home /></main>
+      <Footer />
+    </>
+  )
 }
+
 export default App
