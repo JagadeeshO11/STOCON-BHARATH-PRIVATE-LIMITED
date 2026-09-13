794a244720549ad3124087005917c3bd9b4afb9a
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 11:35:06 +0530

    refactor: consolidate mobile WhatsApp action with bottom navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { BottomNav } from './components/layout/BottomNav'
 import { Footer } from './components/layout/Footer'
 import { Header } from './components/layout/Header'
-import { WhatsAppFloat } from './components/layout/WhatsAppFloat'
 import { AppRoutes } from './app/routes'
 
-function App(){return <BrowserRouter><Header/><main><AppRoutes/></main><Footer/><BottomNav/><WhatsAppFloat/></BrowserRouter>}
+function App(){return <BrowserRouter><Header/><main><AppRoutes/></main><Footer/><BottomNav/></BrowserRouter>}
 export default App
