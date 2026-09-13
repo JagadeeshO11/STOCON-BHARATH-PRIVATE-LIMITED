f8616a21613b3fa9a1fd1c80559010c3228891a1
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 11:08:36 +0530

    feat: add multipage application routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,22 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { PageTransition } from '../components/layout/PageTransition'
+import { About } from '../pages/About/About'
+import { ExportServices } from '../pages/ExportServices/ExportServices'
 import { HomePage } from '../pages/Home/HomePage'
+import { Products } from '../pages/Products/Products'
+import { Quality } from '../pages/Quality/Quality'
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <PageTransition>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/export-services" element={<ExportServices />} />
+        <Route path="/quality" element={<Quality />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </PageTransition>
   )
 }
