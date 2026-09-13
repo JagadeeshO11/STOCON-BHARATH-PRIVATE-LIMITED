cd7767f7ee87ba6fde9f285134efbaa145ae895b
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 11:32:26 +0530

    feat: integrate bottom navigation and WhatsApp action

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import { BrowserRouter } from 'react-router-dom'
+import { BottomNav } from './components/layout/BottomNav'
 import { Footer } from './components/layout/Footer'
 import { Header } from './components/layout/Header'
+import { WhatsAppFloat } from './components/layout/WhatsAppFloat'
 import { AppRoutes } from './app/routes'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <main>
-        <AppRoutes />
-      </main>
-      <Footer />
-    </BrowserRouter>
-  )
-}
-
+function App(){return <BrowserRouter><Header/><main><AppRoutes/></main><Footer/><BottomNav/><WhatsAppFloat/></BrowserRouter>}
 export default App
