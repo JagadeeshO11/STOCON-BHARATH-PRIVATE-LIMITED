cf5d3abe86ce98abbda62846be593bfad97f36cb
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 10:27:49 +0530

    fix: resolve audit issues and validate company contact model

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,31 @@
-import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config(
+export default [
   {
     ignores: ['dist', 'node_modules'],
   },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-)
+]
