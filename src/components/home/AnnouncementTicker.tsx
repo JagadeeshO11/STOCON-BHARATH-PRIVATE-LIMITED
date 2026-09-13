fd413d38942f01f6ca19ce19164686149dc6352f
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-13 10:17:50 +0530

    refactor(home): isolate homepage sections into modules

---
+++ src/components/home/AnnouncementTicker.tsx
@@ -0,0 +1,5 @@
+import './AnnouncementTicker.css'
+
+export function AnnouncementTicker() {
+  return <div className="ticker" aria-label="Product categories">FOOD PRODUCTS <i>✦</i> FRESH FRUITS <i>✦</i> VEGETABLES <i>✦</i> INDIAN SPICES <i>✦</i> GLOBAL EXPORT</div>
+}
