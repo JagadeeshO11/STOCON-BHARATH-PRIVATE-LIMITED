0aba598ec5fc4a79c567bdaa5817cb1023a157eb
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-14 19:39:35 +0530

    Add global floating contact actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { BottomNav } from './components/layout/BottomNav'
 import { Footer } from './components/layout/Footer'
 import { Header } from './components/layout/Header'
+import { WhatsAppFloat } from './components/layout/WhatsAppFloat'
+import { BrochureFloat } from './components/layout/BrochureFloat'
 import { AppRoutes } from './app/routes'
 
-function App(){return <BrowserRouter><Header/><main><AppRoutes/></main><Footer/><BottomNav/></BrowserRouter>}
+function App(){
+  return (
+    <BrowserRouter>
+      <Header/>
+      <main><AppRoutes/></main>
+      <Footer/>
+      <BrochureFloat/>
+      <WhatsAppFloat/>
+      <BottomNav/>
+    </BrowserRouter>
+  )
+}
 export default App
