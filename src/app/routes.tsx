dc8eda163ae7f5ff42f6a9a33d9d4f62da0f8009
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-14 19:29:46 +0530

    Add categories route and dedicated products route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { PageTransition } from '../components/layout/PageTransition'
 import { About } from '../pages/About/About'
+import { Categories } from '../pages/Categories/Categories'
 import { ExportServices } from '../pages/ExportServices/ExportServices'
 import { HomePage } from '../pages/Home/HomePage'
 import { Products } from '../pages/Products/Products'
@@ -13,6 +14,7 @@ export function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/products" element={<Products />} />
         <Route path="/export-services" element={<ExportServices />} />
         <Route path="/quality" element={<Quality />} />
