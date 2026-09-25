6cc55ceca9c864eff271a98e68e958a64d7b5173
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-25 11:42:26 +0530

    Add Services route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,6 +4,7 @@ import { About } from '../pages/About/About'
 import { Categories as Products } from '../pages/Categories/Categories'
 import { HomePage } from '../pages/Home/HomePage'
 import { Contact } from '../pages/Contact/Contact'
+import { Services } from '../pages/Services/Services'
 
 export function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export function AppRoutes() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
