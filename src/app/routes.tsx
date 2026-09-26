88248db9b4e6744d406e7da0f8f4fc281758a2fb
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-26 11:03:21 +0530

    Add brochure route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,6 +5,7 @@ import { Categories as Products } from '../pages/Categories/Categories'
 import { HomePage } from '../pages/Home/HomePage'
 import { Contact } from '../pages/Contact/Contact'
 import { Services } from '../pages/Services/Services'
+import { Brochure } from '../pages/Brochure/Brochure'
 
 export function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ export function AppRoutes() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/brochure" element={<Brochure />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
